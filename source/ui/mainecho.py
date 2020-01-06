a4a625a17bbb0fcebca6a6eaa7a63e681ec17e1b
Brian Gunnison <bgunnison@gmail.com>
2020-01-06 15:56:58 -0800

    UI is starting to do something...

--- source/ui/mainecho.py
+++ source/ui/mainecho.py
@@ -2,18 +2,38 @@ import kivy
 kivy.require('1.0.8')
 
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics import Color, Rectangle
+
+# maybe its cool?
+# https://stackoverflow.com/questions/31179155/how-to-set-a-screen-background-image-in-kivy
+#class RootScreen(ScreenManager):
+#    pass
+
+#class StartScreen(Screen):
+#    pass
+
+class RootWidget(BoxLayout):
+    pass
 
 
-class EchoApp(App):
+class TitleBoxLayout(BoxLayout):
+    pass
+
+class LogoZipped(BoxLayout):
+    pass
+
+class MainEchoApp(App):
     def build(self):
-        g = GridLayout()
-        title = Label(text='Virtual Robot', font_size=120)
-        g.add_widget(title)
+        g = RootWidget() # RootScreen() #
+        title_box = TitleBoxLayout()
+        title_box.add_widget(LogoZipped())
+        g.add_widget(title_box)
         return g
 
 
 
 if __name__ == '__main__':
-    EchoApp().run()
+    MainEchoApp().run()
