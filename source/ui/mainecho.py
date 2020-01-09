988fbf5ec5517e7e820b40f32b3cb9ce634db4c6
bgunnison <bgunnison@gmail.com>
2020-01-08 20:06:37 -0800

    UI - change colors of nav buttons

--- source/ui/mainecho.py
+++ source/ui/mainecho.py
@@ -4,13 +4,11 @@ kivy.require('1.0.8')
 from kivy.app import App
 #from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.bubble import Bubble
 from kivy.uix.button import Button
 from kivy.graphics import Color, Rectangle
 from kivy.properties import ListProperty
+from kivy.utils import get_color_from_hex
 from kivy.config import Config
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'multisamples', 8)
@@ -24,8 +22,8 @@ Config.set('kivy','window_icon','media/icon.ico') # no work...
 
 #class StartScreen(Screen):
 #    pass
-NAV_BUTTON_ON_COLOR = (0.1, 0.1, .2, 1.0)
-NAV_BUTTON_OFF_COLOR = (0, 0, 0, .8)
+NAV_BUTTON_ON_COLOR = (1,1,1,1)  #kivy.utils.get_color_from_hex('#93FFFF')
+NAV_BUTTON_OFF_COLOR = (1, 1, 1, .2)
 nav_button_ids = ['nav_midi', 'nav_echo', 'nav_live', 'nav_help']
 
 class RootWidget(BoxLayout):
@@ -37,8 +35,10 @@ class RootWidget(BoxLayout):
     def nav_button_pressed(self, but, id_key):
         for id in nav_button_ids:
             self.ids[id].background_color = NAV_BUTTON_OFF_COLOR
+            self.ids[id].text = '[color=#04d3ff]' + self.ids[id].text + '[/color]' # close but no cigar, keeps adding...
 
         but.background_color = NAV_BUTTON_ON_COLOR
+        but.text = but.text.replace('#04d3ff', '#FFFFff')
         #print(self.ids.keys())
         print(id_key)
    
