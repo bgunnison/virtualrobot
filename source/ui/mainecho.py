b3e26e5376adb8f4faf967e5fd689cb4deab171a
Brian Gunnison <bgunnison@gmail.com>
2020-01-08 16:03:32 -0800

    UI - more nav

--- source/ui/mainecho.py
+++ source/ui/mainecho.py
@@ -10,6 +10,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.bubble import Bubble
 from kivy.uix.button import Button
 from kivy.graphics import Color, Rectangle
+from kivy.properties import ListProperty
 from kivy.config import Config
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'multisamples', 8)
@@ -23,16 +24,25 @@ Config.set('kivy','window_icon','media/icon.ico') # no work...
 
 #class StartScreen(Screen):
 #    pass
+NAV_BUTTON_ON_COLOR = (0.1, 0.1, .2, 1.0)
+NAV_BUTTON_OFF_COLOR = (0, 0, 0, .8)
+nav_button_ids = ['nav_midi', 'nav_echo', 'nav_live', 'nav_help']
 
 class RootWidget(BoxLayout):
+    
+
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
 
-    def nav_midi(self, but):
-        but.show_arrow = False
-        but.background_color = 1.0, 0.0, 0.0, 1.0
-        print(f'nav midi')
+    def nav_button_pressed(self, but, id_key):
+        for id in nav_button_ids:
+            self.ids[id].background_color = NAV_BUTTON_OFF_COLOR
+
+        but.background_color = NAV_BUTTON_ON_COLOR
+        #print(self.ids.keys())
+        print(id_key)
    
+    
 
 
 class TitleBoxLayout(BoxLayout):
