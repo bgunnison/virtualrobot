8168e15a86a0b060275c6eb5488346eae892f0f9
Brian Gunnison <bgunnison@gmail.com>
2020-02-07 15:03:34 -0800

    copy protection test working

--- source/common/license_generator.py
+++ source/common/license_generator.py
@@ -8,8 +8,10 @@
 
  https://build-system.fman.io/generating-license-keys
 """
+import os
+import sys
 import rsa # pip install rsa
-from base64 import b64encode
+from base64 import b64encode, b64decode                                         
 import argparse
 import logging
 from datetime import datetime
@@ -26,8 +28,10 @@ class License:
         if settings is not None:
             pubkey = settings.get('VirtualRobotPublicKey', '')
 
+        s = b64decode(signature)
+
         try:
-            rsa.verify(email.encode('utf-8'), signature, pubkey)
+            rsa.verify(email.encode('utf-8'), s, pubkey)
         except rsa.VerificationError:
             log.error(f'Invalid key: {email}')
             return
@@ -47,55 +51,64 @@ class Keys:
     def gen(self):
         snow = datetime.now()
         dt = snow.strftime("%m_%d_%Y_%H_%M_%S")
-        self.priv_file = f'VirtualRobotPrivateKey{dt}.pem'
-        self.pub_file = f'VirtualRobotPublicKey{dt}.pem'
+        self.priv_file = f'VirtualRobotPrivateKey_{dt}.pem'
+        self.pub_file = f'VirtualRobotPublicKey_{dt}.pem'
 
-        pubkey, privkey = rsa.newkeys(2048)
+        pubkey, privkey = rsa.newkeys(512)
         pub = pubkey.save_pkcs1().decode('ascii')
         pri = privkey.save_pkcs1().decode('ascii')
         
         f = open(self.priv_file, 'w')
         f.write(pri)
+        log.info(f'Saved private key to file: {os.path.realpath(f.name)}\n')
         f.close()
         f = open(self.pub_file, 'w')
         f.write(pub)
+        log.info(f'Saved public key to file: {os.path.realpath(f.name)}\n')
         f.close()
 
-        print(f'{pub}')
+
+        print(f'{pub}\n')
         print(f'')
-        print(f'{pri}')
+        print(f'{pri}\n')
 
 
-    def load_priv_key(name):
+    def load_priv_key(self, name):
         with open(name, mode='rb') as privatefile:
             keydata = privatefile.read()
         return rsa.PrivateKey.load_pkcs1(keydata)
 
 
-    def load_pub_key(name):
+    def load_pub_key(self, name):
         with open(name, mode='rb') as pubfile:
             keydata = pubfile.read()
         return rsa.PublicKey.load_pkcs1(keydata)
 
-    def sign(Self, message, privkey):
-        signature = rsa.sign(mesage.encode('utf-8'), privkey, 'SHA-1')
+    def sign(self, message, privkey):
+        signature = rsa.sign(message.encode('utf-8'), privkey, 'SHA-1')
         l = b64encode(signature).decode('ascii')
-        print(data + '\n' + l)
+        print(f'{message}\n')
+        print(f'{l}\n')
         return l
 
 
 
 def test_keys():
 
-    log.info('Virtual Robot License generator, do not distribute')
+    log.info('Virtual Robot License generator, do not distribute\n')
 
-    ap = argparse.ArgumentParser(argparse.SUPPRESS)
-    ap.add_argument("-e", "--email", required=True, help="email of user")
-    ap.add_argument("-v", "--privkeyfile", required=True, help="private key file")
-    ap.add_argument("-p", "--pubkeyfile", required=True, help="public key file")
-    args = vars(ap.parse_args())
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-e", "--email", help="email of user", nargs='?', default=None)
+    ap.add_argument("-v", "--privkeyfile", help="private key file", nargs='?', default=None)
+    ap.add_argument("-p", "--pubkeyfile", help="public key file", nargs='?', default=None)
+    args = ap.parse_args()
 
-    if '@' not in args['email']:
+    email = args.email
+
+    if email is None:
+        email = 'fred@boogie.com'
+
+    if '@' not in email:
         log.error(f'No @ in email: {args["email"]}')
         return 
 
@@ -103,18 +116,19 @@ def test_keys():
 
     k.gen()
 
-    if args['privkeyfile'] == None:
+    if args.privkeyfile == None:
         privkf = k.priv_file
 
-    if args['pubkeyfile'] == None:
+    if args.pubkeyfile == None:
         pubkf = k.pub_file
 
-
-
-    sig = k.sign(args['email'], k.load_priv_key(privkf))
+    version = '0.1'
+    serial = 1435  # todo database of emails and serial #s (start big so we look like large sales ;)
+    message = f'VirtualRobot MIDI ECHO Version: {version} SN: {serial} - ' + email
+    sig = k.sign(message, k.load_priv_key(privkf))
 
     l = License()
-    l.verify(args['email'], l, k.load_pub_key(pubkf))
+    l.verify(message, sig, k.load_pub_key(pubkf))
 
 
 
